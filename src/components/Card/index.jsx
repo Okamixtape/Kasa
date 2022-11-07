d7126133e3a01a4eab325058b925e47ef79d679c
Loup Aubour <laubour@arneogroup.com>
2022-11-07 11:51:33 +0100

    KASA-10: Création et intégration des composants Card et Gallery

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import image from "../../assets/homeBanner.png";
+import { NavLink } from "react-router-dom";
 
 export class Card extends Component {
     render() {
         return (
             <div className="card">
-                <img className="card__image" src={ image } alt="Logement" />
-                <p className="card__text">Titre de la location</p>
+                <NavLink to={"/" + this.props.id}>
+                    <div className="card__background"></div>
+                    <img className="card__image" src={this.props.img} alt="Logement" />
+                    <p className="card__text">{this.props.title}</p>
+                </NavLink>
             </div>
         );
     }
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import Card from "../Card/index";
+import data from "../../data/logements.json";
 
 export class Gallery extends Component {
     render() {
+        const housingData = data;
+
         return (
             <div className="gallery">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {housingData.map((housing) => (
+                    <Card
+                        key={housing.id}
+                        id={housing.id}
+                        img={housing.cover}
+                        title={housing.title}
+                    />
+                ))}
             </div>
         );
     }
